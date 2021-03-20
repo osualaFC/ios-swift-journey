3224518744e03d5849a25bd04049e3e8e8ee062a
Fredrick Osuala <fredrick.osuala@gmail.com>
2021-03-20 17:24:11 +0100

    Initial Commit

---
+++ StoryPrompt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryPrompt
+//
+//  Created by fredrick osuala on 20/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
