d6843e255926711d5050660605cf1226ccbabbbc
Fredrick Osuala <fredrick.osuala@gmail.com>
2021-04-17 09:59:24 +0100

    Initial Commit

---
+++ TableViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViews
+//
+//  Created by fredrick osuala on 17/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
